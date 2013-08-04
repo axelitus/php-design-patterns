1e2d5b34a736ef77816fdfbe6b01c0cdf2079b47
Axel Pardemann <axelitusdev@gmail.com>
2013-08-04 16:35:03 -0500

    Fixed the docblocks again.

--- res/tests/axelitus/patterns/tests/Multiton/Concrete.php
+++ res/tests/axelitus/patterns/tests/Multiton/Concrete.php
@@ -7,8 +7,8 @@
  * @author      Axel Pardemann (axelitusdev@gmail.com)
  * @license     MIT License
  * @copyright   2013 - Axel Pardemann
- * @link        http://axelitus.mx/projects/patterns
- * @source      https://github.com/axelitusdev/patterns
+ * @link        http://axelitus.mx/projects/php-patterns
+ * @source      https://github.com/axelitus/php-patterns
  */
 
 namespace axelitus\patterns\tests;
