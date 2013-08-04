429eb6b995863d13d8c1b33815abc7a9c70d5b87
Axel Pardemann <axelitusdev@gmail.com>
2013-08-04 13:11:19 -0500

    Added PHPUnit config files.

---
+++ res/tests/axelitus/patterns/tests/TestCase.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Part of the axelitus\asseter package.
+ *
+ * @package     axelitus\asseter\test
+ * @version     0.1
+ * @author      Axel Pardemann (axelitusdev@gmail.com)
+ * @license     MIT License
+ * @copyright   2013 - Axel Pardemann
+ * @link        http://axelitus.mx/projects/asseter
+ * @source      https://github.com/axelitusdev/asseter
+ */
+
+namespace axelitus\asseter\tests;
+
+class TestCase extends \PHPUnit_Framework_TestCase
+{
+    public function test_default()
+    {
+        $this->assertTrue(true, "This is a default test to see everything is working fine.");
+    }
+}
