157363a422541b1054cae0d7dcf69d89876a0acd
Axel Pardemann <axelitusdev@gmail.com>
2013-08-08 17:58:48 -0500

    Modified the Factory class to be more consistent with the changes introduced in the package.

--- src/axelitus/Patterns/Creational/Factory.php
+++ src/axelitus/Patterns/Creational/Factory.php
@@ -1,14 +1,13 @@
 <?php
 /**
- * Part of the axelitus\patterns package.
+ * Part of composer package: axelitus/patterns
  *
- * @package     axelitus\patterns
+ * @package     axelitus\Patterns
  * @version     0.1
  * @author      Axel Pardemann (axelitusdev@gmail.com)
  * @license     MIT License
  * @copyright   2013 - Axel Pardemann
- * @link        http://axelitus.mx/projects/php-patterns
- * @source      https://github.com/axelitus/php-patterns
+ * @link        http://axelitus.mx/projects/axelitus/patterns
  */
 
 namespace axelitus\Patterns\Creational;
@@ -16,59 +15,20 @@ namespace axelitus\Patterns\Creational;
 use axelitus\Patterns\Interfaces;
 
 /**
- * class Factory
+ * Class Factory
  *
- * A base class to implement the Factory Design Pattern.
+ * Sets the template for a factory.
  *
- * @package axelitus\patterns
- * @since       0.1     introduced class Factory
- * @uses    interfaces\Forgeable
+ * @package axelitus\Patterns\Creational
  */
 abstract class Factory
 {
     /**
-     * Determines if the given instance is an instanceof the given interfaces\Forgeable class.
+     * Builds a new object
      *
-     * The function ensures that the given class exists and implements interfaces\Forgeable, if not then it fails
-     * and throws an InvalidArgumentException.
+     * @param mixed $params,...     The parameters to build an object.
      *
-     * @static
-     * @since       0.1     introduced public static function instance_of(interfaces\Forgeable $instance, $class)
-     * @param   Interfaces\Forgeable $instance   The instance to check if is instanceof class
-     * @param string $class      The full qualified class name (FQCN) to check if instance is instanceof it
-     * @return  bool    Whether the given $instance is instanceof $class
-     * @throws \InvalidArgumentException
+     * @return mixed    The newly created object.
      */
-    public static function instance_of(Interfaces\Forgeable $instance, $class)
-    {
-        if (($forgeable = __NAMESPACE__ . '..\Interfaces\Forgeable') == $class) {
-            return true;
-        }
-
-        if (!class_exists($class) and !interface_exists($class)) {
-            throw new \InvalidArgumentException('The given $class [' . $class . '] does not exist.');
-        }
-
-        $implements = class_implements($class);
-        if (!in_array($forgeable, $implements)) {
-            throw new \InvalidArgumentException('The given $class [' . $class . '] does not implement ' . __NAMESPACE__ . "\\interfaces\Forgeable.");
-        }
-
-        return ($instance instanceof $class);
-    }
-
-    //endregion
-
-    //region Methods/Functions
-
-    /**
-     * Builds a new interfaces\Forgeable object
-     *
-     * @since       0.1     introduced abstract public function build($params = null)
-     * @param mixed $params,...     The parameters to build a interfaces\Forgeable object
-     * @return  Interfaces\Forgeable   The newly forged instnace.
-     */
-    abstract public function produce($params = null);
-
-    //endregion
+    abstract public function make();
 }
