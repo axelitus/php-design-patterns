a088944fd9ca8e9c967fe602a93e8f5b6095cf70
Axel Pardemann <axelitusdev@gmail.com>
2013-08-08 17:57:01 -0500

    Fixced the exceptions file name and changed the docblocks.

---
+++ src/axelitus/Patterns/Exceptions/MethodNotAllowedException.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Part of composer package: axelitus/patterns
+ *
+ * @package     axelitus\Patterns
+ * @version     0.1
+ * @author      Axel Pardemann (axelitusdev@gmail.com)
+ * @license     MIT License
+ * @copyright   2013 - Axel Pardemann
+ * @link        http://axelitus.mx/projects/axelitus/patterns
+ */
+
+namespace axelitus\Patterns\Exceptions;
+
+/**
+ * Class MethodNotAllowedException
+ *
+ * The exception to throw when a method is not allowed.
+ *
+ * @package axelitus\Patterns\Exceptions
+ */
+class MethodNotAllowedException extends \RuntimeException
+{
+}
