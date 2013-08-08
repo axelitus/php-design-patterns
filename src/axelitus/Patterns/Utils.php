6cb8246444eb89e6b5f9ff6ca620d95a0bbbc32d
Axel Pardemann <axelitusdev@gmail.com>
2013-08-08 12:49:24 -0500

    Fixed an error witht the class_implements function where false was never returned.

--- src/axelitus/Patterns/Utils.php
+++ src/axelitus/Patterns/Utils.php
@@ -1,14 +1,13 @@
 <?php
 /**
- * Part of the axelitus\patterns package.
+ * Part of composer package: axelitus/patterns
  *
- * @package     axelitus\patterns
+ * @package     axelitus\Patterns
  * @version     0.1
  * @author      Axel Pardemann (axelitusdev@gmail.com)
  * @license     MIT License
  * @copyright   2013 - Axel Pardemann
- * @link        http://axelitus.mx/projects/php-patterns
- * @source      https://github.com/axelitus/php-patterns
+ * @link        http://axelitus.mx/projects/axelitus/patterns
  */
 
 namespace axelitus\Patterns;
@@ -18,20 +17,20 @@ namespace axelitus\Patterns;
  *
  * A utilities class
  *
- * @package axelitus\patterns
+ * @package     axelitus\patterns
  * @since       0.1     introduced class Singleton
  */
 final class Utils
 {
     /**
-     * Determines whether a class implements an interface or not. If no interface is given, the class returns
-     * the result of the call to class_implements($class)
+     * Determines whether a class implements an interface or not.
      *
-     * @since       0.1     introduced final public static function class_implements($class, $interface = null)
-     * @param mixed     $class      An object (class instance) or a string (class name).
-     * @param string     $interface     The full qualified class name (FQCN) that represents an interface.
-     * @return array|bool   true or false depending if the class implements the given interface.
-     *                      if no interface is given (= null) then an array on success, or FALSE on error.
+     * If no interface is given, the class returns the result of the call to class_implements($class)
+     *
+     * @param object|string $class     An object (class instance) or a string (class name).
+     * @param string        $interface The full qualified class name (FQCN) that represents an interface.
+     *
+     * @return array|bool true or false depending if the class implements the given interface. If no interface is given (null) then an array on success, or FALSE on error.
      */
     final public static function class_implements($class, $interface = null)
     {
@@ -40,6 +39,8 @@ final class Utils
         if (!is_null($interface)) {
             if ($implements !== false and in_array($interface, $implements)) {
                 $implements = true;
+            } else {
+                $implements = false;
             }
         }
 
