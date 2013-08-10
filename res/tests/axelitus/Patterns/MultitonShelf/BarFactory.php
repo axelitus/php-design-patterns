8f85d0a53bb1967bc25d91de8f15bf9b77d7b332
Axel Pardemann <axelitusdev@gmail.com>
2013-08-10 00:15:32 -0500

    Introduced the MultitonShelf which is a non-static multiton pattern similar implementation using Factory Dependency Injection.

---
+++ res/tests/axelitus/Patterns/MultitonShelf/BarFactory.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Part of composer package: axelitus/patterns
+ *
+ * @package     axelitus\Patterns
+ * @version     0.2
+ * @author      Axel Pardemann (axelitusdev@gmail.com)
+ * @license     MIT License
+ * @copyright   2013 - Axel Pardemann
+ * @link        http://axelitus.mx/projects/axelitus/patterns
+ */
+
+namespace axelitus\Patterns\MultitonShelf;
+
+use axelitus\Patterns\Creational;
+
+/**
+ * Class BarFactory
+ *
+ * @package axelitus\Patterns\MultitonShelf
+ */
+class BarFactory extends Creational\Factory
+{
+    /**
+     * Builds a new object
+     *
+     * @param mixed $params,...     The parameters to build an object.
+     *
+     * @return mixed    The newly created object.
+     */
+    public function make()
+    {
+        $value = func_get_arg(0);
+        return Bar::forge($value);
+    }
+
+}
---
+++ res/tests/axelitus/Patterns/TestMultitonShelf.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Part of composer package: axelitus/patterns
+ *
+ * @package     axelitus\Patterns
+ * @version     0.2
+ * @author      Axel Pardemann (axelitusdev@gmail.com)
+ * @license     MIT License
+ * @copyright   2013 - Axel Pardemann
+ * @link        http://axelitus.mx/projects/axelitus/patterns
+ */
+
+namespace axelitus\Patterns;
+
+
+use axelitus\Patterns\MultitonShelf;
+
+/**
+ * Class TestMultitonShelf
+ *
+ * @package axelitus\patterns\tests
+ */
+class TestMultitonShelf extends TestCase
+{
+    /**
+     * @type MultitonShelf $shelf The singleton shelf instance.
+     */
+    protected $shelf;
+
+    public function setUp()
+    {
+        $this->shelf = MultitonShelf\BarShelf::forge(new MultitonShelf\BarFactory());
+    }
+
+    public function test_grab()
+    {
+        $foo = $this->shelf->grab('foo', 'foo');
+        $this->assertEquals('foo', $foo->getValue());
+
+        $foo2 = $this->shelf->grab('bar', 'bar');
+        $this->assertEquals('bar', $foo2->getValue());
+
+        $foo3 = $this->shelf->grab('foo', 'bar');
+        $this->assertEquals('foo', $foo3->getValue());
+    }
+}
